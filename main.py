760c36c5753781ae6e2f6fc2895c4c56658ef9be
sharif <61516383+sharifhsn@users.noreply.github.com>
2021-03-21 23:00:43 -0400

    ok

--- main.py
+++ main.py
@@ -1,293 +1,5 @@
-# import random
-#
-# # space object
-# class Space:
-#     def __init__(self, val):
-#         self = val
-#
-# # info object
-# class SpaceQuery:
-#     def __init__(self, isSafe, surroundingMines, neighbor_safe, neighbor_mine, neighbor_unqueried):
-#         self.isSafe = isSafe
-#         self.surroundingMines = surroundingMines
-#         self.neighbor_safe = neighbor_safe
-#         self.neighbor_mine = neighbor_mine
-#         self.neighbor_unqueried = neighbor_unqueried
-#
-# # check_nearest finds how many mines are next to a specified position on the board
-# # board is a list of list of spaces
-# # x is the row we are searching
-# # y is the column we are searching
-# def check_nearest(board, x, y):
-#     n = 0
-#     try:
-#         if board[x][y + 1] == -1:
-#             n += 1
-#     except IndexError:
-#         pass
-#     try:
-#         if board[x][y - 1] == -1:
-#             n += 1
-#     except IndexError:
-#         pass
-#     try:
-#         if board[x - 1][y - 1] == -1:
-#             n += 1
-#     except IndexError:
-#         pass
-#     try:
-#         if board[x - 1][y + 1] == -1:
-#             n += 1
-#     except IndexError:
-#         pass
-#     try:
-#         if board[x - 1][y] == -1:
-#             n += 1
-#     except IndexError:
-#         pass
-#     try:
-#         if board[x + 1][y - 1] == -1:
-#             n += 1
-#     except IndexError:
-#         pass
-#     try:
-#         if board[x + 1][y + 1] == -1:
-#             n += 1
-#     except IndexError:
-#         pass
-#     try:
-#         if board[x + 1][y] == -1:
-#             n += 1
-#     except IndexError:
-#         pass
-#     return n
-# def board_gen(d, n):
-#     # simple generation of all 0s
-#     board = []
-#     for i in range(d):
-#         r = []
-#         for j in range(d):
-#             s = Space(False, 0)
-#             r.append(s)
-#         board.append(r)
-#
-#     # add mines
-#     while n > 0:
-#         #print(n)
-#         i = random.randint(0, d - 1)
-#         j = random.randint(0, d - 1)
-#         if board[i][j] == 0:
-#             board[i][j] = -1
-#             n -= 1
-#             #print_board(board)
-#
-#     # environment
-#     for i in range(d):
-#         for j in range(d):
-#             if board[i][j] != -1:
-#                 n = check_nearest(board, i, j)
-#                 board[i][j] = n
-#                 print_board(board)
-#
-#     return board
-#
-# def print_board(board):
-#     print("printing board")
-#     for r in board:
-#         for s in r:
-#             if s == -1:
-#                 print("M ", end='')
-#             else:
-#                 print(str(s) + " ", end='')
-#         print()
-#
-# def update_info(board, info, unvisited, stk, x, y):
-#     # updates info based on what's around it
-#     if board[x][y] == info[x][y].neighbor_unqueried:
-#         # all hidden spaces are mines
-#         try:
-#             if info[x][y + 1].isSafe == 0:
-#                 info[x][y + 1].isSafe = -1
-#                 unvisited.remove((x, y + 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x][y - 1].isSafe == 0:
-#                 info[x][y - 1].isSafe = -1
-#                 unvisited.remove((x, y - 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x - 1][y - 1].isSafe == 0:
-#                 info[x - 1][y - 1].isSafe = -1
-#                 unvisited.remove((x - 1, y - 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x - 1][y + 1].isSafe == 0:
-#                 info[x - 1][y + 1].isSafe = -1
-#                 unvisited.remove((x - 1, y + 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x - 1][y].isSafe == 0:
-#                 info[x - 1][y].isSafe = -1
-#                 unvisited.remove((x - 1, y))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x + 1][y - 1].isSafe == 0:
-#                 info[x][y + 1].isSafe = -1
-#                 unvisited.remove((x + 1, y - 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x + 1][y + 1].isSafe == 0:
-#                 info[x + 1][y + 1].isSafe = -1
-#                 unvisited.remove((x + 1, y + 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x + 1][y].isSafe == 0:
-#                 info[x + 1][y].isSafe = -1
-#                 unvisited.remove((x + 1, y))
-#         except IndexError:
-#             pass
-#     elif board[x][y] + info[x][y].neighbor_unqueried == 8:
-#         # all hidden spaces are safe
-#         try:
-#             if info[x][y + 1].isSafe == 0:
-#                 info[x][y + 1].isSafe = 1
-#                 stk.append((x, y + 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x][y - 1].isSafe == 0:
-#                 info[x][y - 1].isSafe = 1
-#                 stk.append((x, y - 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x - 1][y - 1].isSafe == 0:
-#                 info[x - 1][y - 1].isSafe = 1
-#                 stk.append((x - 1, y - 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x - 1][y + 1].isSafe == 0:
-#                 info[x - 1][y + 1].isSafe = 1
-#                 stk.append((x - 1, y + 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x - 1][y].isSafe == 0:
-#                 info[x - 1][y].isSafe = 1
-#                 stk.append((x - 1, y))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x + 1][y - 1].isSafe == 0:
-#                 info[x + 1][y - 1].isSafe = 1
-#                 stk.append((x + 1, y - 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x + 1][y + 1].isSafe == 0:
-#                 info[x + 1][y + 1].isSafe = 1
-#                 stk.append((x + 1, y + 1))
-#         except IndexError:
-#             pass
-#         try:
-#             if info[x + 1][y].isSafe == 0:
-#                 info[x + 1][y].isSafe = 1
-#                 stk.append((x + 1, y))
-#         except IndexError:
-#             pass
-# def basic_agent(board):
-#     counter = 0
-#     info = []
-#     for i in range(len(board)):
-#         r = []
-#         for j in range(len(board)):
-#             sq = SpaceQuery(0, 0, 0, 0, 8)
-#             r.append(sq)
-#         info.append(r)
-#
-#     # list of positions that are definitely known
-#     safe = []
-#     mines = []
-#     unvisited = []
-#     stk = []
-#
-#     # populate unvisited
-#     for i in range(len(board)):
-#         for j in range(len(board)):
-#             unvisited.append((i, j))
-#
-#     while len(unvisited) > 0:
-#         # v is a random unvisited space
-#         while True:
-#             v = random.randint(0, len(unvisited) - 1)
-#             # make sure that the random hidden space isn't known to be a mine
-#             if info[x][y].isSafe != -1:
-#                 break
-#         x = v[0]
-#         y = v[1]
-#         # stk will contain all safe, unvisited searches
-#         while True:
-#             unvisited.remove(x, y)
-#             if board[x][y] == -1:
-#                 # oops, mine exploded
-#                 counter += 1
-#                 info[x][y].isSafe = -1
-#                 mines.append((x, y))
-#             else:
-#                 # okay, no mine here
-#                 info[x][y].isSafe = 1
-#                 safe.append((x, y))
-#
-#                 # will update spaces around
-#                 update_info(board, info, unvisited, stk, x, y)
-#             if len(stk) > 0:
-#                 p = stk.pop()
-#                 x = p[0]
-#                 y = p[1]
-#             else:
-#                 break
-# def jav_basic_agent(board, minesWalkedInto, unvisited, visited, safe, unsafe):
-#     info = []
-#     for i in range(len(board)):
-#         r = []
-#         for j in range(len(board)):
-#             sq = SpaceQuery(0, 0, 0, 0, 8)
-#             r.append(sq)
-#         info.append(r)
-#
-#     while len(unvisited) > 0:
-#         int x = random.randInt(0, len(unvisited))
-#         coord = unvisited[x]
-#
-# def queryCellBasic(board, info, unvisited, visited, coord):
-#     if info[coord[0]][coord[1]].isSafe != 0:
-#         return
-#
-#     unvisited.remove(coord)
-#     visited.append(coord)
-#
-#     if board[coord[0]][coord[1]] == -1
-#
-#
-#
-# d = 5
-# n = 10
-# print_board(board_gen(d, n))
-
-
-
-
-
 import random
-
+import matplotlib
 class Query:
     def __init__(self, isSafe, surroundingMines, neighbor_safe, neighbor_mine, neighbor_unqueried):
             self.isSafe = isSafe
@@ -436,13 +148,13 @@ def print_info(info):
             elif j.isSafe == -1:
                 print("M ", end="")
             else:
-                print(str(j.neighbor_safe) + " ", end="")
+                print(str(j.neighbor_mine) + " ", end="")
             # j is a Query
             # if status is unknown, print period
             # if is mine, print M
             # else print surrounding_mines
         print()
-def basic_agent(board):
+def basic_agent(board, n):
     # initialize info
     # info is a list of list of Query
     info = []
@@ -480,16 +192,19 @@ def basic_agent(board):
         while len(safe) > 0:
             print(safe)
             c = safe.pop()
-            x = c[0]
-            y = c[1]
 
             # query the cell
-            query_cell_basic(board, info, coord, safe, visited, unvisited, tripped)
+            query_cell_basic(board, info, c, safe, visited, unvisited, tripped)
 
             print("tripped is " + str(tripped))
+    print("final score is " + str(n - len(tripped)))
+    print_board(board)
+    print_info(info)
             
 def query_cell_basic(board, info, coord, safe, visited, unvisited, tripped):
-    unvisited.remove(coord)
+    if coord in unvisited:
+        unvisited.remove(coord)
+        print(str(coord) + " removed from unvisited")
     visited.append(coord)
     x = coord[0]
     y = coord[1]
@@ -502,10 +217,12 @@ def query_cell_basic(board, info, coord, safe, visited, unvisited, tripped):
     print_info(info)
     # update cell info and append newly safe
     update_cell_info(board, info, safe, unvisited, x, y)
-    check_nearest(board, info, safe, unvisited, x, y, update_cell_info)
+    #check_nearest(board, info, safe, unvisited, x, y, update_cell_info)
 # resets the information for a cell and checks all its neighbors once more to update the information
 # returns the unqueried spaces next to it that are known to be safe
 def update_cell_info(board, info, safe, unvisited, x, y):
+    if tuple((x, y)) in unvisited:
+        return
     q = info[x][y]
     print("updating info on (" + str(x) + ", " + str(y) + ")")
     neighbors = 8
@@ -626,7 +343,7 @@ def update_cell_info(board, info, safe, unvisited, x, y):
     # change unqueried neighbors if you know for sure that they're all safe/mines
     # additionally, if unqueried are safe, append for search
     # and if unqueried are mines, remove from unvisited (as we don't want to search them)
-    if q.neighbor_unqueried == board[x][y]:
+    if q.neighbor_unqueried != 0 and q.neighbor_unqueried == board[x][y] - q.neighbor_mine:
         print("oops, all mines")
         for u in qunq:
             info[u[0]][u[1]].isSafe = -1
@@ -639,33 +356,10 @@ def update_cell_info(board, info, safe, unvisited, x, y):
             if u in unvisited:
                 safe.append(u)
             #print("safe is now " + str(safe))
+    print("after update, safe is " + str(safe))
     print_info(info)
-board = board_gen(6, 2)
+
+n = 2
+board = board_gen(4, n)
 print_board(board)
-basic_agent(board)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+basic_agent(board, n)
